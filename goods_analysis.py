ea66115133b74d0117945aa76e89278278139ba5
Karim <karimhamid@yandex.ru>
2020-10-22 16:48:24 +0300

    Последняя задача

--- goods_analysis.py
+++ goods_analysis.py
@@ -1,2 +1,5 @@
 def goods_analysis(*args, in_sale=lambda x: "молоко" in x["название"].lower()):
-    return sorted(filter(in_sale, args), key=lambda x: x['цена'])[:3]
+    return sorted(
+        sorted(filter(in_sale, args), key=lambda x: x['цена'])[:3],
+        key=lambda x: x['цена']
+    )
